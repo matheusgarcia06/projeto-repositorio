642af7c87148ae035906102f58e25b3fff85cc4a
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-04-25 18:28:11 -0300

    feat: adicionando a biblioteca styled-components e realizando o reset global do projeto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyle from './styles/global';
 
 import Main from './pages/Main';
 import Repositorio from './pages/Repositorio';
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route exact path='/' element={<Main />} />
-				<Route
-					exact
-					path='/repositorio/:repositorio'
-					element={<Repositorio />}
-				/>
-			</Routes>
-		</BrowserRouter>
+		<>
+			<GlobalStyle />
+			<BrowserRouter>
+				<Routes>
+					<Route exact path='/' element={<Main />} />
+					<Route
+						exact
+						path='/repositorio/:repositorio'
+						element={<Repositorio />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 
