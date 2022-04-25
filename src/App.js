d2e49771dad4d6d5eaa6e332509e3690430b356f
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-04-25 17:19:58 -0300

    feat: adicionando rotas no projeto, com router dom v6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repositorio from './pages/Repositorio';
+
 function App() {
 	return (
-		<div>
-			<h1>Projeto</h1>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route exact path='/' element={<Main />} />
+				<Route
+					exact
+					path='/repositorio/:repositorio'
+					element={<Repositorio />}
+				/>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
---
+++ src/pages/Repositorio/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Repositorio() {
+	return <h1>Repositorio</h1>;
+}
+
+export default Repositorio;
