c52d605fdc45acb5eb61b1198e224190fe71cc4d
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-04-26 09:55:00 -0300

    feat: Instalando a biblioteca axios, para realizar a requisição HTTP e melhorando a estilização no botão, quando aguarda a requisição ser realizada, criando uma animação

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,47 @@
-import React from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import React, { useState, useCallback } from 'react';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
+import api from '../../services/api';
+
 function Main() {
+	const [newRepo, setNewRepo] = useState('');
+	const [repositorios, setRepositorios] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	const handleSubmit = useCallback(
+		(e) => {
+			e.preventDefault();
+
+			async function submit() {
+				setLoading(true);
+				try {
+					const response = await api.get(`repos/${newRepo}`);
+
+					const data = {
+						name: response.data.full_name,
+					};
+
+					setRepositorios([...repositorios, data]);
+
+					setNewRepo('');
+				} catch (error) {
+					console.log(error);
+				} finally {
+					setLoading(false);
+				}
+			}
+
+			submit();
+		},
+		[newRepo, repositorios]
+	);
+
+	function handleInputChange(e) {
+		setNewRepo(e.target.value);
+	}
+
 	return (
 		<Container>
 			<h1>
@@ -11,11 +49,20 @@ function Main() {
 				Meus Repositórios
 			</h1>
 
-			<Form onSubmit={() => {}}>
-				<input type='text' placeholder='Adicionar Repositórios' />
+			<Form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					placeholder='Adicionar Repositórios'
+					value={newRepo}
+					onChange={handleInputChange}
+				/>
 
-				<SubmitButton>
-					<FaPlus color='#FFF' size={14} />
+				<SubmitButton loading={loading ? 1 : 0}>
+					{loading ? (
+						<FaSpinner color='#FFF' size={14} />
+					) : (
+						<FaPlus color='#FFF' size={14} />
+					)}
 				</SubmitButton>
 			</Form>
 		</Container>
